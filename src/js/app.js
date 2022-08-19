e72a07e894b0a386245c7e1772a617846930fe28
Doris <sayitloud15@gmail.com>
2022-08-19 13:16:24 +0200

    installed webpack

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import './../assets/styles/scss/main.scss';
+
+
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
+console.log("hallo welt");
